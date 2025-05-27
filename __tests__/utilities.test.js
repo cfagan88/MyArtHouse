dcdac94fe34c8ba8a5f98757b14d4d6b98a150b4
Chris Fagan <cfagan88@hotmail.co.uk>
2025-05-27 18:50:02 +0100

    Add additional test for utils function

--- __tests__/utilities.test.js
+++ __tests__/utilities.test.js
@@ -16,4 +16,17 @@ describe("getNewPage function", () => {
     expect(getNewPage(200, -25)).toBe(175);
     expect(getNewPage(3, -2)).toBe(1);
   });
+
+  test("Returns current page if second argument is 0", () => {
+    expect(getNewPage(1, 0)).toBe(1);
+    expect(getNewPage(5, 0)).toBe(5);
+  });
+
+  test("Does not mutate arguments", () => {
+    const currentPage = 5;
+    const change = 3;
+    getNewPage(currentPage, change);
+    expect(currentPage).toBe(5);
+    expect(change).toBe(3);
+  });
 });
