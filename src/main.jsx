9efdb2b951d8f6533701374d532515e43dd4013c
Chris Fagan <cfagan88@hotmail.co.uk>
2025-05-30 20:53:34 +0100

    Update vite config

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/MyArtHouse">
+  <BrowserRouter basename="/MyArtHouse/">
     <App />
   </BrowserRouter>
 );
