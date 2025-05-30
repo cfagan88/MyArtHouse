9aea4fd7d543f1dca529ddd55de3d2a24b4310dc
Chris Fagan <cfagan88@hotmail.co.uk>
2025-05-30 20:21:47 +0100

    Update vite config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/MyArtHouse",
+  base: "/MyArtHouse/",
   plugins: [react(), tailwindcss()],
 });
