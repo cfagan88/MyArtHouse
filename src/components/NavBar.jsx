6e9e9e02dc49ba50a232ed84ce5ec1e87a1ea736
Chris Fagan <cfagan88@hotmail.co.uk>
2025-06-03 19:02:51 +0200

    Fixes Home link reloading whole page

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,25 +7,12 @@ function NavBar() {
     <nav className="fixed top-0 w-full z-40 bg-[rgba(10, 10, 10, 0.8)] backdrop-blur-lg border-b border-white/10 shadow-lg">
       <div className="max-w-5xl px-4">
         <div className="flex justify-start items-center h-16 space-x-8">
-          <Link
-            to="/"
-            onClick={(e) => {
-              if (location.pathname === "/") {
-                window.location.reload();
-              }
-            }}
-            className="w-10 h-10 block"
-          >
+          <Link to="/" className="w-10 h-10 block">
             <img className="w-full h-full object-contain" src={Logo} />
           </Link>
           <div className="flex items-center space-x-8">
             <Link
               to="/"
-              onClick={(e) => {
-                if (location.pathname === "/") {
-                  window.location.reload();
-                }
-              }}
               className="text-gray-300 hover:text-white transition-colors"
             >
               Home
